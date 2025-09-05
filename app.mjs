dc8094fd7a5eb306d474bbc374f0e324fcc019b7
Gjyr <rryryry@gmail.com>
2025-09-05 20:08:09 +0300

    <setup> update route paths

--- app.mjs
+++ app.mjs
@@ -5,8 +5,9 @@ import { LOCAL_ADDRESS } from "#config";
 export default async function app(req, res) {
   const url = new URL(req.url, `http://${LOCAL_ADDRESS}/`);
 
-  if (url.pathname.startsWith("/hold/v1/i"))
+  if (url.pathname.startsWith("/api/v1/hold/i"))
     await serveImages(url.searchParams.get("img"), res);
-  if (url.pathname.startsWith("/log/v1")) await loggerRout(req, res, url);
+  else if (url.pathname.startsWith("/api/v1/log"))
+    await loggerRout(req, res, url);
   else await serveSiteFiles(req, res);
 }
