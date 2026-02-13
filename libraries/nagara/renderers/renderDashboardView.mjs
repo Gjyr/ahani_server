e2aedb1769d84ca83ae8f0d50f6164a603055636
Gjyr <rryryry@gmail.com>
2026-02-13 14:38:10 +0300

    <refactor> delete comments

--- libraries/nagara/renderers/renderDashboardView.mjs
+++ libraries/nagara/renderers/renderDashboardView.mjs
@@ -2,21 +2,6 @@ import { renderDashboard } from "../templates/dashboard.mjs";
 import * as nagara from "../index.mjs";
 
 export async function renderDashboardView(req, res) {
-  // if (!playerId) {
-  //     res.writeHead(401, {
-  //         'Content-Type': 'text/html',
-  //         'Content-Length': Buffer.byteLength(unauthorizedTemplate())
-  //     });
-  //     res.end(unauthorizedTemplate());
-  //     return true;
-  // }
-
-  // const isValid = await nagara.validatePlayerToken(playerId);
-  // if (!isValid) {
-  //     res.writeHead(403, { 'Content-Type': 'text/html' });
-  //     res.end(forbiddenTemplate());
-  //     return true;
-  // }
   const playerId = req.headers["x-player-id"];
 
   if (!playerId) {
